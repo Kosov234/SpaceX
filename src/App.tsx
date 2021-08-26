02964d5d4483172fcc60ad7b7949d2ccb977fafb
Alexandr Cosov <kosovkosov@gmail.com>
2021-08-26 14:16:48 +0200

    Fetching users implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql",
+  cache: new InMemoryCache(),
+});
+
+interface user {
+  name: string;
+  id: string;
+}
 
 function App() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      users {
+        name
+        id
+      }
+    }
+  `);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      {data.users.map(({ name, id }: user) => (
+        <div key={id}>
+          <p>{name}</p>
+        </div>
+      ))}
+    </ApolloProvider>
   );
 }
 
